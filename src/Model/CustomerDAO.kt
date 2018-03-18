f2eb0e260b53348770301da2501e470a8293f473
PABLO <fernandez.pablo44@gmail.com>
2018-03-18 00:33:46 -0300

    read customers table test

--- src/Model/CustomerDAO.kt
+++ src/Model/CustomerDAO.kt
@@ -1,4 +1,29 @@
 package Model
 
+import Service.SqliteManager
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+
 class CustomerDAO {
+
+    fun readAllCustomers(): ArrayList<Customer>{
+
+        //TEST THIS METHOD
+        val customers : ArrayList<Customer> = ArrayList()
+        val sqliteManager: SqliteManager = SqliteManager.instance
+        sqliteManager.connect()
+        var preparedStatement : PreparedStatement =
+                sqliteManager.connection!!.prepareStatement("SELECT * FROM Customer")
+        val resultSet: ResultSet = preparedStatement.executeQuery()
+
+        while (resultSet.next()){
+            customers.add(Customer(resultSet.getLong("dni"),resultSet.getString("name")))
+        }
+
+        sqliteManager.closeConnection()
+        resultSet.close()
+
+        return customers
+    }
+    
 }
