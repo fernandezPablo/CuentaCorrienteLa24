6a6a4e815172e0682c75a1c191fbde5e610057ea
PABLO <fernandez.pablo44@gmail.com>
2018-04-22 00:12:04 -0300

    ToolTipText added to the components. Replaced JTextField for JFormatedTextField in DNI and YEAR

--- src/View/NewCustomerView.kt
+++ src/View/NewCustomerView.kt
@@ -1,12 +1,29 @@
 package View
 
 import java.awt.*
+import java.awt.event.FocusEvent
+import java.awt.event.FocusListener
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
+import java.text.Format
 import javax.swing.*
 import javax.swing.border.TitledBorder
 import javax.swing.table.DefaultTableModel
 
-class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
+class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title), FocusListener
 {
+    override fun focusLost(e: FocusEvent?) {
+        if (e?.source == this.vehicleYearTextField ){
+            println(this.vehicleYearTextField.value)
+        }
+    }
+
+    override fun focusGained(e: FocusEvent?) {
+        if(e?.source == this.vehicleYearTextField){
+            this.vehicleYearTextField.text = ""
+        }
+    }
+
     private val mainPanel : JPanel = JPanel(GridBagLayout())
     private val title : JLabel = JLabel("REGISTRO DE NUEVOS CLIENTES")
     private val customerDataPanel: JPanel = JPanel(GridBagLayout())
@@ -14,11 +31,11 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
     private val registerButton : JButton = JButton("REGISTRAR")
     private val cancelButton : JButton = JButton("CANCELAR")
     private val customerNameTextField : JTextField = JTextField()
-    private val customerDniTextField : JTextField = JTextField()
+    private val customerDniTextField : JFormattedTextField = JFormattedTextField(Integer(0))
     private val vehicleDomainTextField : JTextField = JTextField()
     private val vehicleManufacturerTextField : JTextField = JTextField()
     private val vehicleVersionTextField : JTextField = JTextField()
-    private val vehicleYearTextField : JTextField = JTextField()
+    private val vehicleYearTextField : JFormattedTextField = JFormattedTextField(Integer(1991))
     private val addVehicleButton : JButton = JButton("AGREGAR")
     private val vehiclesTable : JTable = JTable()
     private val vehiclesTableScroll : JScrollPane = JScrollPane(this.vehiclesTable)
@@ -26,6 +43,7 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
 
     init {
         this.initComponents()
+        this.vehicleYearTextField.addFocusListener(this)
     }
 
     private fun initComponents(){
@@ -70,6 +88,7 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
         gbc.weightx = 1.0
         this.customerNameTextField.border = TitledBorder("NOMBRE Y APELLIDO")
         this.customerNameTextField.font = Font("Arial",Font.BOLD,25)
+        this.customerNameTextField.toolTipText = "NOMBRE Y APELLIDO DEL CLIENTE"
         this.customerDataPanel.add(this.customerNameTextField,gbc)
 
         /*CUSTOMER DNI TEXTFIELD IN CUSTOMER DATA PANEL*/
@@ -81,6 +100,7 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
         gbc.weightx = 1.0
         this.customerDniTextField.border = TitledBorder("DNI")
         this.customerDniTextField.font = Font("Arial",Font.BOLD,25)
+        this.customerDniTextField.toolTipText = "NUMERO DE DOCUMENTO DEL CLIENTE"
         this.customerDataPanel.add(this.customerDniTextField,gbc)
 
         /*VEHICLE DATA PANEL*/
@@ -95,8 +115,6 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
         this.vehicleDataPanel.border = TitledBorder("DATOS DE LOS VEHICULOS")
         this.mainPanel.add(this.vehicleDataPanel,gbc)
 
-
-
         /*ADD VEHICLE BUTTON IN VEHICLE DATA PANEL*/
         gbc = GridBagConstraints()
         gbc.gridx = 1
@@ -106,6 +124,7 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
         gbc.gridwidth = 1
         gbc.gridheight = 1
         gbc.anchor = GridBagConstraints.BELOW_BASELINE_TRAILING
+        this.addVehicleButton.toolTipText = "AGREGAR UN VEHICULO"
         this.vehicleDataPanel.add(this.addVehicleButton,gbc)
 
         /*VEHICLE DOMAIN TEXTFIELD IN VEHICLE DATA PANEL*/
@@ -118,6 +137,8 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
         gbc.gridheight = 1
         gbc.weightx = 1.0
         this.vehicleDomainTextField.border = TitledBorder("DOMINIO")
+        this.vehicleDomainTextField.font = Font("Arial",Font.BOLD,20)
+        this.vehicleDomainTextField.toolTipText = "DOMINIO DEL VEHICULO"
         this.vehicleDataPanel.add(this.vehicleDomainTextField,gbc)
 
         /*VEHICLE MANUFACTURED TEXTFIELD IN VEHICLE DATA PANEL*/
@@ -130,6 +151,8 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
         gbc.gridheight = 1
         gbc.weightx = 1.0
         this.vehicleManufacturerTextField.border = TitledBorder("FABRICANTE")
+        this.vehicleManufacturerTextField.font = Font("Arial",Font.BOLD,20)
+        this.vehicleManufacturerTextField.toolTipText = "FABRICANTE DEL VEHICULO"
         this.vehicleDataPanel.add(this.vehicleManufacturerTextField,gbc)
 
         /*VEHICLE VERSION TEXTFIELD IN VEHICLE DATA PANEL*/
@@ -142,6 +165,8 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
         gbc.gridheight = 1
         gbc.weightx = 1.0
         this.vehicleVersionTextField.border = TitledBorder("MODELO")
+        this.vehicleVersionTextField.font = Font("Arial",Font.BOLD,20)
+        this.vehicleVersionTextField.toolTipText = "VERSION DEL VEHICULO"
         this.vehicleDataPanel.add(this.vehicleVersionTextField,gbc)
 
         /*VEHICLE YEAR TEXTFIELD IN VEHICLE DATA PANEL*/
@@ -154,6 +179,8 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
         gbc.gridheight = 1
         gbc.weightx = 1.0
         this.vehicleYearTextField.border = TitledBorder("AÑO")
+        this.vehicleYearTextField.font = Font("Arial",Font.BOLD,20)
+        this.vehicleYearTextField.toolTipText = "AÑO DE FABRICACION DEL VEHICULO. INGRESAR SOLO NUMEROS"
         this.vehicleDataPanel.add(this.vehicleYearTextField,gbc)
 
         /*VEHICLE SCROLL TABLE IN VEHICLE DATA PANEL*/
@@ -182,6 +209,7 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
         gbc.gridheight = 1
         gbc.weighty = 0.0
         gbc.anchor = GridBagConstraints.BELOW_BASELINE_TRAILING
+        this.registerButton.toolTipText = "REGISTRAR CLIENTE"
         this.mainPanel.add(this.registerButton,gbc)
 
         /*CANCEL BUTTON*/
@@ -193,9 +221,11 @@ class NewCustomerView(owner: Frame?, title: String?) : JDialog(owner, title)
         gbc.gridheight = 1
         gbc.weighty = 0.0
         gbc.anchor = GridBagConstraints.BELOW_BASELINE_LEADING
+        this.cancelButton.toolTipText = "CANCELAR EL REGISTRO DEL CLIENTE"
         this.mainPanel.add(this.cancelButton,gbc)
 
         this.pack()
     }
+
 
 }
